20feebe20d5826dad70092dd4c68f6153801b404
FatimahMohamed <fatimah1pm@gmail.com>
2025-06-12 10:09:11 +0100

    Add Enter key functionality for answer submission and update game initialization

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,13 @@ document.addEventListener("DOMContentLoaded", function () {
         });    
 
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
     runGame("addition");      
 });   
 
@@ -24,7 +31,10 @@ document.addEventListener("DOMContentLoaded", function () {
  *and after the user's answer has been processed.
  */
 function runGame (gameType) {
-    let num1 = Math.floor(Math.random() * 25) + 1;
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+    
+    num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
